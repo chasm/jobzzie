20bb436752e1dbc9ccbdec46bc06127306295b3a
chasm <charles.munat@gmail.com>
2013-07-27 18:35:20 -0700

    Added controllers, models, routes, blank views. Good luck

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,33 @@
 Jobzzie::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
   
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # Employer routes
+  get "employer_profile" => "employer#index", as: :employer_profile
+  get "search_for_students" => "employer#search", as: :search_for_students
+  
+  # Student routes
+  get "student_profile" => "student#index", as: :student_profile
+  get "search_for_openings" => "student#search", as: :search_for_openings
+  
+  # Openings (CRUD)
+  resources :openings
+  
+  # Register with site after email validation
+  get  "register/:code" => "registration#new", as: :register
+  post "register/:code" => "registration#create"
+  
+  # Log in or out (login form is on home page)
+  post   "login"  => "session#create"
+  get    "logout" => "session#destroy"
+  delete "logout" => "session#destroy"
+  
+  # Reset password after email validation
+  get   "reset/:code" => "password#edit", as: :reset
+  put   "reset/:code" => "password#update"
+  patch "reset/:code" => "password#update"
+  
+  # Site routes
+  get "privacy" => "site#privacy"
+  get "terms" => "site#terms"
+  
+  root 'site#index' # Login/signup/reset form
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Registrant.destroy_all
+User.destroy_all
+Category.destroy_all
+School.destroy_all
+
+Registrant.create({
+  email: "joe@mama.com"
+})
+
+school = School.create({
+  name: "UCLA",
+  zip_code: "90210"
+})
+
+categories = Category.create([
+  { name: "Accounting" },
+  { name: "Rocket Science" },
+  { name: "Pizza Throwing" }
+])
+
+student = Student.create({
+  name_first: "Joe",
+  name_last: "Student",
+  email: "joe@student.com",
+  password: "123",
+  password_confirmation: "123"
+})
+
+job = Job.create({
+  title: "Pizza thrower",
+  started_on: Date.today - 1.year,
+  ended_on: Date.today - 4.months,
+  student: student
+})
+
+study = Study.create({
+  started_on: Date.today - 1.year,
+  ended_on: Date.today - 4.months,
+  did_graduate: false,
+  major: "Underwater Basketweaving",
+  minor: "Girls",
+  school: school,
+  student: student
+})
+
+employer = Employer.create({
+  name_of_company: "Bob's Pizza",
+  email: "bob@bobspizza.com",
+  password: "123",
+  password_confirmation: "123"
+})
+
+location = Location.create({
+  street_1: "123 Elm St.",
+  zip_code: "90401",
+  employer: employer
+})
+
+opening = Opening.create({
+  title: "Underwater basketweaver",
+  starts_on: Date.today,
+  description: "You know what it is.",
+  employer: employer,
+  location: location,
+  categories: categories
+})
+
